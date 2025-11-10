7abd83a4c7aa3307c1ad33ec03d7c1314152a529
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-11-11 02:43:39 +0530

    connectioncreated

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//middleware to parse json data
+app.use(express.json());
+
+const blog = require('./routes/blog');
+//mount the blog routes
+app.use('/api/v1',blog)
+
+const connectWithDb = require('./config/database');
+connectWithDb();
+
+app.listen(PORT, () => {
+    console.log(`APP is running on ${PORT}`);
+})
+
+app.get("/",(req, res) =>{ 
+    res.send(`<h1>Welcome to blog site homepage<h1>`)
+})
